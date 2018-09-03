82f10d53f792998d141cdc7d344882c99af4002c
孙鑫磊 <sunxl@cnjiang.com>
2018-09-03 19:48:38 +0800

    switch已基本完成

---
+++ components/vi_switch/vi_switch.js
@@ -0,0 +1,38 @@
+Component({
+  properties: {
+    activeText: {
+      type: String,
+      vlaue: ''
+    },
+    inactiveText: {
+      type: String,
+      value: ''
+    },
+    disable: {
+      type: Boolean,
+      value: false
+    },
+    checked: {
+      type: Boolean,
+      value: false
+    },
+    exterior: {
+      type: Boolean,
+      value: false
+    }
+  },
+  options: {
+    multipleSlots: true
+  },
+  data: {
+  },
+
+  methods: {
+    triggerToChange() {
+      if (this.data.disable) return false
+      this.setData({
+        checked: !this.data.checked
+      })
+    }
+  }
+})
--- pages/vi_switch/vi_switch.js
+++ pages/vi_switch/vi_switch.js
@@ -1,66 +1,9 @@
 // pages/vi_switch/vi_switch.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    switch1Checked: true,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  switch1Change: function (e) {
+    console.log('switch发生change事件', e.detail.value)
   }
 })
