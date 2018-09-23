5aa824b6fc0949271c6c1f7eb88b76a7216c191b
孙鑫磊 <sunxl@cnjiang.com>
2018-09-23 17:46:29 +0800

    add viAbnormalTips

---
+++ example/pages/viAbnormalTips/index.js
@@ -0,0 +1,26 @@
+Page({
+  data: {
+    tipsArr: [
+      '啊哦，暂时还没有内容',
+      '联系下客服试试吧'
+    ],
+    networkTipsArr: [
+      '请关闭手机网络链接,会出现按钮'
+    ]
+  },
+  triggerToRouter() {
+    console.log('triggerToRouter')
+    wx.switchTab({
+      url: '/pages/index/index',
+    })
+  },
+  triggerToRefresh({ detail }) {
+    console.log(detail)
+  },
+  networkChange({ detail }) {
+    this.data.networkTipsArr[1] = '当前网络环境：' + detail.networkType
+    this.setData({
+      networkTipsArr: this.data.networkTipsArr
+    })
+  }
+})
