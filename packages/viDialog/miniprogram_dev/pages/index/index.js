38fd87e690f233f588fee730d9e3e7ee424085f3
孙鑫磊 <sunxl@cnjiang.com>
2018-10-19 10:02:35 +0800

    --

---
+++ packages/viDialog/miniprogram_dev/pages/index/index.js
@@ -0,0 +1,34 @@
+Page({
+  data: {
+    defaultIsHide: false,
+    confirmIsHide: false,
+    alertIsHide: false,
+    customIsHide: false,
+    inputIsHide: false
+  },
+  triggerToShowDefaultDialog () {
+    this.setData({
+      defaultIsHide: !this.data.defaultIsHide
+    })
+  },
+  triggerToShowConfirmDialog () {
+    this.setData({
+      confirmIsHide: !this.data.confirmIsHide
+    })
+  },
+  triggerToShowAlertDialog () {
+    this.setData({
+      alertIsHide: !this.data.alertIsHide
+    })
+  },
+  triggerToShowCustomDialog () {
+    this.setData({
+      customIsHide: !this.data.customIsHide
+    })
+  },
+  triggerToShowInputDialog() {
+    this.setData({
+      inputIsHide: !this.data.inputIsHide
+    })
+  }
+})
