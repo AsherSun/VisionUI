708cb6b46b2da36c4a922d94c20117c6914f774c
孙鑫磊 <sunxl@cnjiang.com>
2018-11-09 15:37:24 +0800

    revise vi-motion components

--- src/components/vi-motion/vi-motion.js
+++ src/components/vi-motion/vi-motion.js
@@ -3,15 +3,54 @@ Component({
     ishide: {
       type: Boolean,
       value: false,
+      observer(newValue) {
+        if (this.data.outHasAnimate) {
+          if (newValue) {
+            this.setData({
+              isShow: newValue
+            });
+          } else {
+            setTimeout(() => {
+              this.setData({
+                isShow: newValue
+              });
+            }, this.data.animationDuration * 1000)
+          }
+        } else {
+          this.setData({
+            isShow: newValue
+          });
+        }
+      }
     },
-    animateName: {
+    enterAnimateName: {
       type: String,
       value: 'bounce'
+    },
+    outAnimateName: {
+      type: String,
+      value: ''
+    },
+    isMaskHide: {
+      type: Boolean,
+      value: false
+    },
+    outHasAnimate: {
+      type: Boolean,
+      value: true
+    },
+    animationDuration: {
+      type: Number,
+      value: 1
     }
   },
   methods: {
     triggerToHide(e) {
+      if (this.data.isMaskHide) return false;
       this.triggerEvent('hide', this.data.ishide)
     }
   },
+  data: {
+    isShow: false
+  }
 })
